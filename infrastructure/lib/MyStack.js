92631284dae20c6f38d6ff6be2240878ac8724a6
Bobby Ross <bobby.dev@pm.me>
2020-10-11 15:18:30 +0100

    build: add cdk config for app

---
+++ infrastructure/lib/MyStack.js
@@ -0,0 +1,9 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Define your stack
+  }
+}
